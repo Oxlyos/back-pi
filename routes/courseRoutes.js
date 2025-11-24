7ff63ef086654a868889081a5eed11f84017bf6a
Oxlyos <bmaamoun6@gmail.com>
2025-11-24 23:52:39 +0100

    Fix ReferenceError in courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const CourseController = require('../controllers/courseController');
+
 router.get('/:id', CourseController.getCourse);
 
 module.exports = router;
